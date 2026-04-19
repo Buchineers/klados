1b0f9679139fa676fe039f0b0ef95030ebd8d9ad
Constructor <fractalminds@protonmail.com>
2026-04-19 03:28:48 +0200

    Binary Build Targets

---
+++ src/bin/optil_heuristic.rs
@@ -0,0 +1,28 @@
+use klados_core::{Instance, Tree};
+use pace26io::binary_tree::IndexedBinTreeBuilder;
+use pace26io::pace::simplified::Instance as PaceInstance;
+use std::io::{self, BufReader};
+
+#[path = "../commands/mod.rs"]
+mod commands;
+
+fn read_instance() -> Result<Instance, Box<dyn std::error::Error>> {
+    let stdin = io::stdin();
+    let reader = BufReader::new(stdin.lock());
+    let mut builder = IndexedBinTreeBuilder::default();
+    let pace = PaceInstance::try_read(reader, &mut builder)?;
+
+    let num_leaves = pace.num_leaves as u32;
+    let trees: Vec<Tree> = pace
+        .trees
+        .iter()
+        .map(|t| Tree::from_cursor(t.top_down(), num_leaves))
+        .collect();
+
+    Ok(Instance::new(trees, num_leaves))
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let instance = read_instance()?;
+    commands::heuristic::run(&instance, "auto", false)
+}
